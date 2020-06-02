7cf5093371666df448dec210e33afcf845b6de1f
hyukim <woo1038@hanmail.net>
2020-06-02 16:50:16 +0900

    no message

--- scroll.js
+++ scroll.js
@@ -1,54 +1,52 @@
 window.addEventListener('scroll', function() {
-    let a = document.documentElement.scrollTop;
-    
+    let $scroll = document.documentElement.scrollTop;
+    const $long = document.getElementById('long');
+
     let r = parseInt(255);
     let g = parseInt(20);
     let b = parseInt(20);
 
-    document.getElementById('long').style.background = 
-    'rgb(' + r + ',' + g + ',' + b + ')';
+    $long.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
     
     /* red, orange, yellow */
-    if(a > 100 && a < 2000) {
+    if($scroll > 100 && $scroll < 2000) {
         r = parseInt(255);
-        g = parseInt(a/7);
+        g = parseInt($scroll/7);
         b = parseInt(20);
     
-        document.getElementById('long').style.background = 
-        'rgb(' + r + ',' + g + ',' + b + ')';
-        console.log(a);   
-    } /* green */
-    else if(a >= 2000 && a < 3000) {
-        r = parseInt(390-a/12);
+        $long.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';  
+    } 
+    /* green */
+    else if($scroll >= 2000 && $scroll < 3000) {
+        r = parseInt(390-$scroll/12);
         g = parseInt(255);
         b = parseInt(20);
     
-        document.getElementById('long').style.background = 
-        'rgb(' + r + ',' + g + ',' + b + ')';
-    } /* sky */
-    else if(a >= 3000 && a < 3900) {
-        r = parseInt(340-a/15);
-        g = parseInt(255-a/100);
-        b = parseInt((a-2950)/3);
+        $long.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
+    } 
+    /* sky */
+    else if($scroll >= 3000 && $scroll < 3900) {
+        r = parseInt(340-$scroll/15);
+        g = parseInt(255-$scroll/100);
+        b = parseInt(($scroll-2950)/3);
         
         
-        document.getElementById('long').style.background = 
-        'rgb(' + r + ',' + g + ',' + b + ')';
-    } /* blue */
-    else if(a >= 3900 && a < 4800) {
+        $long.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
+    } 
+    /* blue */
+    else if($scroll >= 3900 && $scroll < 4800) {
     r = parseInt(20);
-    g = parseInt(1200-a/4);
+    g = parseInt(1200-$scroll/4);
     b = parseInt(255);
 
-    document.getElementById('long').style.background = 
-    'rgb(' + r + ',' + g + ',' + b + ')';
-    } /* violet */
-    else if(a >= 4800) {
-        r = parseInt((a-4600)/5);
-        g = parseInt(1200-a/4);
+    $long.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
+    } 
+    /* violet */
+    else if($scroll >= 4800) {
+        r = parseInt(($scroll-4600)/5);
+        g = parseInt(1200-$scroll/4);
         b = parseInt(255);
     
-        document.getElementById('long').style.background = 
-        'rgb(' + r + ',' + g + ',' + b + ')';
-        }
+        $long.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
 })
